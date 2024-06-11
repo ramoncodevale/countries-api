b646b696a297bed5e3bbd5664d40875ef80d344d
ramoncodevale <ramonvalentim88@gmail.com>
2024-06-11 15:14:22 -0300

    chore: add initial tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(209, 23%, 22%)',
+        'very-dark-background': 'hsl(207, 26%, 17%)',
+        'very-dark-blue': 'hsl(200, 15%, 8%)',
+        'dark-gray-input':  'hsl(0, 0%, 52%)',
+        'very-dark-light': 'hsl(0, 0%, 98%)',
+        'white-dark-mode': 'hsl(0, 0%, 100%)',
+    },
+    fontFamily: 'Nunito Sans, sans-serif',
+  },
+  plugins: [],
+}
+}
+
