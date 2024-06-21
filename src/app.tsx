540f35eca9bb2673cb6d6e616428292bd95b481a
ramoncodevale <ramonvalentim88@gmail.com>
2024-06-21 15:16:09 -0300

    feat: add changes files

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/home/home";
 import { Navbar } from "./components/navbar/navbar";
-import { CountryDetail } from "./components/detail/country-detail";
-import { ThemeProvider } from "./context/theme-provider";
+// import { CountryDetail } from "./components/detail/country-detail";
+// import { ThemeProvider } from "./context/theme-provider";
 
 
 export function App(){
  
   return(
-    <>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <div className="bg-very-dark-light dark:bg-very-dark-background">
+    {/* <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> */}
    <Navbar />
     <Routes> 
    <Route path="/" element={<Home/>} />
-   <Route path="/country/:name" element={<CountryDetail/>}/>
+   {/* <Route path="/country/:name" element={<CountryDetail/>}/> */}
    </Routes>
-   </ThemeProvider>
-    </>
+   {/* </ThemeProvider> */}
+    </div>
 
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import { App } from './app'
+import { BrowserRouter } from 'react-router-dom'
+import { DarkModeProvider } from './context/dark-mode-context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <DarkModeProvider>  
+    <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>
+    </DarkModeProvider>
+  </React.StrictMode>
 )
